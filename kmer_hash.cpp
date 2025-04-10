91319b5e6e66808ca669a0aa379445a4b3072b97
Ben Krohn-Hansen <benkrohnhansen@gmail.com>
2025-04-09 18:43:28 -0700

    HM

--- kmer_hash.cpp
+++ kmer_hash.cpp
@@ -48,19 +48,14 @@ int main(int argc, char** argv) {
 
     // Load factor of 0.5
     size_t hash_table_size = n_kmers * (1.0 / 0.5);
-    upcxx::global_ptr<HashMap> my_map = upcxx::new_<HashMap>(hash_table_size);
+    HashMap hashmap(hash_table_size);
 
     std::vector<upcxx::global_ptr<HashMap>> all_maps(upcxx::rank_n());
 
+    all_maps[upcxx::rank_me()] = hashmap.self_ptr;
+
     for (int i = 0; i < all_maps.size(); ++i) {
         BUtil::print("Rank", upcxx::rank_me(), "sees all_maps[", i, "] =", all_maps[i]);
-    }   
-
-
-    all_maps[upcxx::rank_me()] = my_map;
-
-    for (int i = 0; i < upcxx::rank_n(); i++) {
-        all_maps[i] = upcxx::broadcast(all_maps[i], i).wait();
     }
 
     BUtil::print("Rank %i and table size %i\n", upcxx::rank_me(), hash_table_size);
@@ -83,7 +78,7 @@ int main(int argc, char** argv) {
     std::vector<kmer_pair> start_nodes;
 
     for (auto& kmer : kmers) {
-        bool success = hashmap.insert(kmer, all_maps);
+        bool success = hashmap.insert(kmer);
         if (!success) {
             BUtil::print("INSERT WAS NOT A SUCCESS\n");
             throw std::runtime_error("Error: HashMap is full!");
@@ -95,8 +90,6 @@ int main(int argc, char** argv) {
     }
     auto end_insert = std::chrono::high_resolution_clock::now();
     upcxx::barrier();
-
-    BUtil::print("FINISHED INSERT");
 
     double insert_time = std::chrono::duration<double>(end_insert - start).count();
     if (run_type != "test") {
